e96146d2c0677d673fe3077d7fadc77c2eb3d884
Helloguru499 <60912676+Helloguru499@users.noreply.github.com>
2022-06-12 15:16:09 -0500

    Update fil-url.sh

--- fil-url.sh
+++ fil-url.sh
@@ -17,12 +17,12 @@ gau $domain --o $domain
 END=$(date +%s)
 
 timeexec=$(($END - $START))
-echo  -e "\033[31m******************GAU  worked on target for $timeexec seconds **********************\033[m"
+echo  -e "\033[31m******************GAU worked on target for $timeexec seconds **********************\033[m"
 
 waybackurls $domain |tee -a $domain|qsreplace > $domain
 endtimes=$(date +%s)
 timeexecs=$(($endtimes - $START))
-echo -e "\033[32m*****************WAYBACKURLS  worked on target for $timeexecs seconds********************\033[m"
+echo -e "\033[32m*****************WAYBACKURLS worked on target for $timeexecs seconds********************\033[m"
 
 cat $domain|grep "="|egrep -iv ".(css|jpg|gif|js|jpeg|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt)"|qsreplace|cut -d "?" -f 2|egrep -iv http|sort -u > $domain-param
 sed  -i 's/&/\n/g' $domain-param
@@ -33,7 +33,7 @@ cat $domain |grep $i|head -n 1 >> $domain-final
 done
 rm $domain
 rm $domain-param
-sort -u $domain-final -o $domain-final
+sort -u $domain-final -o $domain
 
-cat $domain-final
+cat $domain
 rm $domain-final
